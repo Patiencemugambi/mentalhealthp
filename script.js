d8b6d0d77de815e5b3713f30e6d2ef57dd482a9f
Patiencemugambi <patiencemugambi@gmail.com>
2023-05-26 22:31:25 +0300

    added js file for functionality

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// Function to handle "Learn More" button click
-document.getElementById('btn2').addEventListener('click', function() {
-    alert("Button clicked!"); // Display an alert when the button is clicked
+// Get the form element by its class name
+const form = document.querySelector('.contact-form');
+
+// Add an event listener to the form's submit event
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Display a success message
+    alert('Form submitted successfully!');
+
+    // Optionally, you can reset the form after displaying the alert
+    form.reset();
 });
