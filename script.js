33149522a184b55759d8e22de201e4906d759bdf
Patiencemugambi <patiencemugambi@gmail.com>
2023-05-26 20:39:17 +0300

    added js file for functionality

---
+++ script.js
@@ -0,0 +1,4 @@
+// Function to handle "Learn More" button click
+document.getElementById('btn2').addEventListener('click', function() {
+    alert("Button clicked!"); // Display an alert when the button is clicked
+});
